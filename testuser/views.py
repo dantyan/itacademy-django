cf1d28055c2698a1881ce1110cc6243d55804a11
Dan Tyan <dan.tyan@gmail.com>
2019-01-31 19:07:03 +0600

    test user model

--- testuser/views.py
+++ testuser/views.py
@@ -1,9 +1,25 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from testuser.models import TestUser
 
 
 def users(request):
-    testusers = TestUser.objects.all()
+    testusers = TestUser.objects.filter(
+        username__icontains='fose',
+        username__contains='maria',
+        is_active=True
+    ).exclude(
+        Q(username__istartswith='maria') | Q(username__iendswith='maria')
+    ).order_by('-id')
 
-    return render(request, 'testuser/home.html')
+    print(testusers.query)
+
+    testusers = TestUser.objects.filter(
+        Q(username__icontains='maria') | Q(username__icontains='fose'),
+        is_active=True
+    ).order_by('-id')
+
+    return render(request, 'maria/home.html', {
+        'users': testusers
+    })
