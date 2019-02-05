732d13c3a5a0302158dd5cde95d61f20b8bdf113
Dan Tyan <dan.tyan@gmail.com>
2019-02-05 20:54:40 +0600

    user auth

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import LoginView, home
+from user.views import LoginView, home, logout
 
 app_name = 'user'
 urlpatterns = [
     path('', home, name='home'),
     path('login/', LoginView.as_view(), name='user-login'),
+    path('logout/', logout, name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth import views as auth_views
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def home(request):
     return render(request, 'profile/home.html')
 
 
+def logout(request):
+    request.session.flush()
+    return redirect('/user/')
+
+
 class LoginView(auth_views.LoginView):
     template_name = 'profile/login.html'
+
+# class LogoutView(auth_views.LogoutView):
+#     template_name = 'user/logout.html'
