d58b11469388341ea4084c974661fe01727c3591
Dan Tyan <dan.tyan@gmail.com>
2019-03-05 20:36:42 +0600

    user registration

--- itacademy/urls.py
+++ itacademy/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('articles/', include('articles.urls'))
 """
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -22,14 +21,11 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testuser/', include('testuser.urls', namespace='testuser')),
-]
-
-urlpatterns += i18n_patterns(
     path('', include('main.urls', namespace='main')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('forum/', include('forum.urls', namespace='forum')),
     path('user/', include('user.urls', namespace='user')),
-)
+]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from user.views import LoginView, home, logout
+from user.views import LoginView, SignUpConfirmView, SignupDoneView, SignupView, home, logout
 
 app_name = 'user'
 urlpatterns = [
     path('', home, name='home'),
     path('login/', LoginView.as_view(), name='user-login'),
     path('logout/', logout, name='logout'),
+
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('signup/done/', SignupDoneView.as_view(), name='signup-done'),
+    re_path(
+        r'^signup/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        SignUpConfirmView.as_view(),
+        name='signup-confirm',
+    ),
+
 ]
