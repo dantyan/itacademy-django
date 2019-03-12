a6be276454eaff643ac96270af918f81937882d2
Dan Tyan <dan.tyan@gmail.com>
2019-03-12 20:54:47 +0600

    ajax

--- forum/views/ajax.py
+++ forum/views/ajax.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 from forum.models import Post
 
 
-# def ajax_view(request):
-#     post = Post.objects.order_by('?').first()
-#
-#     return JsonResponse({
-#         'title': post.title,
-#         'id': post.pk,
-#     })
+def ajax_view(request):
+    post = Post.objects.order_by('?').first()
+
+    return JsonResponse({
+        'title': post.title,
+        'id': post.pk,
+        'content': post.content,
+    })
 
 # def ajax_view(request):
 #     return HttpResponse('text response')
@@ -17,8 +18,8 @@ from forum.models import Post
 # def ajax_view(request):
 #     return HttpResponse('<h1 class="text-right">H1 ajax response</h1>')
 
-def ajax_view(request):
-    post = Post.objects.order_by('?').first()
-    return render(request, 'pages/block/ajax-post.html', {
-        'post': post
-    })
+# def ajax_view(request):
+#     post = Post.objects.order_by('?').first()
+#     return render(request, 'pages/block/ajax-post.html', {
+#         'post': post
+#     })
